9c7514fcaf91f38babee60aa2f2e24cfcd996e67
4nd2in <andrin.weiler@smoca.ch>
2024-06-20 12:46:03 +0200

    Fix

--- src/ch/smoca/redux/Store.kt
+++ src/ch/smoca/redux/Store.kt
@@ -2,6 +2,7 @@ package ch.smoca.redux
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
