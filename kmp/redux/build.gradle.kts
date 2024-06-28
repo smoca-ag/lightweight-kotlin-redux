ea5d03c1bfb32a21272e1aebee59815b653e1ca7
Oli <oliver.mannhart@smoca.ch>
2024-06-28 15:44:02 +0200

    version to 6.1.0 - add middlewares

--- kmp/redux/build.gradle.kts
+++ kmp/redux/build.gradle.kts
@@ -57,7 +57,7 @@ mavenPublishing {
     coordinates(
         groupId = "ch.smoca.lib",
         artifactId = "lightweight-kotlin-redux",
-        version = "6.0.0"
+        version = "6.1.0"
     )
     configure(
         KotlinMultiplatform(
