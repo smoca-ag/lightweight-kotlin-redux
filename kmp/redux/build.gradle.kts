34a856c89ebc847d382fc8b282262e78e9dfd7e8
4nd2in <andrin.weiler@smoca.ch>
2025-04-24 11:47:28 +0200

    bump

--- kmp/redux/build.gradle.kts
+++ kmp/redux/build.gradle.kts
@@ -68,7 +68,7 @@ mavenPublishing {
     coordinates(
         groupId = "ch.smoca.lib",
         artifactId = "lightweight-kotlin-redux",
-        version = "6.2.0"
+        version = "6.2.1"
     )
     configure(
         KotlinMultiplatform(
